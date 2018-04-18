8d2931de1acad3154c071d6f49b8b7bad015ad92
Toprak <toprakademkoc360@gmail.com>
2018-04-18 15:51:14 +0300

    Initial commit

---
+++ deep-net.py
@@ -0,0 +1,103 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def setLearningRate(weight):
+	global new_learning_rate
+	global n_samples
+	# if(weight > 1.0):
+	# new_learning_rate = .0001
+	# else:
+	if(tf.is_nan(weight) == True):
+		exit()
+	else:
+		# if(weight < 0):
+		# 	# positiveWeight = weight * -1
+		# 	# return/
+		# else:
+		new_learning_rate = np.abs((0.001 / n_samples) / weight)
+		# print new_learning_rate
+		# return;
+	return
+
+def isEnough():
+	global enough
+	enough += 1
+	print '\n',enough,'\n'
+	
+def oldArgs(cost,weight,bias):
+	global oldCost
+	global oldWeight
+	global oldBias
+	oldCost = cost
+	oldWeight = weight
+	oldBias = bias
+	# print  oldCost, oldWeight, oldBias, '\n'
+	return;
+
+enough = 0
+oldCost = 0
+oldWeight = 0
+oldBias = 0 
+rnd = np.random
+
+readCSV = pd.read_csv('train1.csv',delimiter=',', names=['x_axis','y_axis'])
+# print readCSV
+readCSV = readCSV[0:250]
+train_X = readCSV.x_axis
+train_Y = readCSV.y_axis
+n_samples = train_X.shape[0]
+new_learning_rate = 0.001 / n_samples
+print new_learning_rate
+learning_rate = tf.placeholder('float')
+training_epochs = n_samples
+display_step = 1
+
+X = tf.placeholder('float')
+Y = tf.placeholder('float')
+
+w = tf.Variable(np.random.uniform(-float((n_samples/2)),float(n_samples)), name='weight')
+b = tf.Variable(0.0, name='bias')
+
+pred = tf.add(tf.multiply(X,w),b)
+cost = tf.reduce_sum(tf.pow(pred-Y,2))/(2*n_samples)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+
+	sess.run(init)
+	print 'Weight', sess.run(w)
+	# file_writer = tf.summary.FileWriter('/Users/jotform/Desktop/toprak/tensorflow/', sess.graph)
+
+	for epoch in range (training_epochs):
+
+		optimizer = tf.train.GradientDescentOptimizer(learning_rate=new_learning_rate).minimize(cost)
+		
+		for (x,y) in zip(train_X,train_Y):
+			sess.run(optimizer, feed_dict={X: train_X, Y: train_Y,learning_rate: new_learning_rate})
+			sess.run(cost, feed_dict={X: train_X, Y: train_Y,learning_rate: new_learning_rate})
+
+		if (epoch+1) % display_step == 0 or (epoch+1) <= 10:
+			c = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
+			print('Epoch:', '%07d' % (epoch+1), 'Cost=', '%07f' % c, \
+                'Weight=', '%07f' % sess.run(w), 'Bias=', '%07f' % sess.run(b),'Learnin-Rate=', new_learning_rate)
+
+		if('%07f' % sess.run(cost, feed_dict={X: train_X, Y:train_Y}) == oldCost or '%07f' % sess.run(w) == oldWeight or '%07f' % sess.run(b) == oldBias):
+			print '\n',sess.run(cost, feed_dict={X: train_X, Y:train_Y}) ,'==', oldCost ,'and', sess.run(w) ,'==', oldWeight ,'and', sess.run(b) ,'==', oldBias
+			isEnough()
+
+
+		if enough >= 3:
+			break
+
+		setLearningRate(sess.run(w))
+		oldArgs('%07f' % sess.run(cost, feed_dict={X: train_X, Y:train_Y}),'%07f' % sess.run(w),'%07f' % sess.run(b))
+
+	training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
+	print "Training cost=", training_cost, "W=", sess.run(w), "b=", sess.run(b), '\n'
+	plt.plot(train_X, train_Y, 'ro', label='Original data')
+	plt.plot(train_X, sess.run(w) * train_X + sess.run(b), label='Fitted line')
+	plt.legend()
+	plt.show()
