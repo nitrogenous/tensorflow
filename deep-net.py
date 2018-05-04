c35f99a4000d729baa07c08cb624b946660cbe5a
Toprak <toprakademkoc360@gmail.com>
2018-05-04 16:34:42 +0300

    Fixed optimizer bug and added color to lines

--- deep-net.py
+++ deep-net.py
@@ -6,6 +6,7 @@ import math
 import matplotlib  
 matplotlib.use('TkAgg') #for macOs 'python is not a framework error'
 import matplotlib.pyplot as plt  
+from colorama import Fore, Back, Style
 
 def setLearningRate(weight):
 	global new_learning_rate
@@ -13,13 +14,13 @@ def setLearningRate(weight):
 	if(tf.is_nan(weight) == True):
 		exit()
 	else:
-		new_learning_rate = np.abs((0.0001 / n_samples) / weight)
+		new_learning_rate = np.abs((0.01 / n_samples) / weight)
 	return
 
 def isEnough():
 	global enough
 	enough += 1
-	print '\n',enough,'\n'
+	print '\n',Fore.RED,enough,Fore.RESET,'\n'
 	
 def oldArgs(cost,weight,bias):
 	global oldCost
@@ -64,8 +65,8 @@ oldWeight = 0
 oldBias = 0 
 rnd = np.random
 
-readCSV = pd.read_csv('spreadsheet.csv',delimiter=',', names=['x_axis','y_axis'])
-readCSV = readCSV[1:100]
+readCSV = pd.read_csv('train1.csv',delimiter=',', names=['x_axis','y_axis'])
+readCSV = readCSV[1:250]
 readCSV = readCSV.sort_values(['x_axis','y_axis'])
 readCSV = readCSV.reset_index(drop=True)
 outliersIndex = cleanList(zip(readCSV.x_axis,readCSV.y_axis),zip(outlierLimits(readCSV.x_axis),outlierLimits(readCSV.y_axis)))
@@ -82,15 +83,15 @@ for i in range(0,(len(outliersIndex))):
 	train = train.drop(outliersIndex[i])
 
 outliers = pd.DataFrame(outliers)
-print outliers
+# print outliers
 
 
 train_X = train.x_axis
 train_Y = train.y_axis
 n_samples = train_X.shape[0]
-new_learning_rate = 0.001 / n_samples
+new_learning_rate = 0.01 / n_samples
 learning_rate = tf.placeholder('float')
-training_epochs = 25
+training_epochs = 9999999
 display_step = 1
 
 X = tf.placeholder('float')
@@ -103,6 +104,7 @@ pred = tf.add(tf.multiply(X,w),b)
 cost = tf.reduce_sum(tf.pow(pred-Y,2))/(2*n_samples)
 
 init = tf.global_variables_initializer()
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=new_learning_rate).minimize(cost)
 
 with tf.Session() as sess:
 
@@ -111,7 +113,6 @@ with tf.Session() as sess:
 
 	for epoch in range (training_epochs):
 
-		optimizer = tf.train.GradientDescentOptimizer(learning_rate=new_learning_rate).minimize(cost)
 		
 		for (x,y) in zip(train_X,train_Y):
 			sess.run(optimizer, feed_dict={X: train_X, Y: train_Y,learning_rate: new_learning_rate})
@@ -119,8 +120,8 @@ with tf.Session() as sess:
 
 		if (epoch+1) % display_step == 0 or (epoch+1) <= 10:
 			c = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
-			print('Epoch:', '%07d' % (epoch+1), 'Cost=', '%07f' % c, \
-                'Weight=', '%07f' % sess.run(w), 'Bias=', '%07f' % sess.run(b),'Learnin-Rate=', new_learning_rate)
+			print Fore.RED,'Epoch:' ,Fore.RESET, '%07d' % (epoch+1),Fore.BLUE ,'Cost:' ,Fore.RESET,'%07f' % c, \
+                Fore.GREEN , 'Weight:',Fore.RESET, '%07f' % sess.run(w), Fore.CYAN , 'Bias:',Fore.RESET, '%07f' % sess.run(b),Fore.YELLOW,'Learnin-Rate:',Fore.RESET, new_learning_rate
 
 		if('%07f' % sess.run(cost, feed_dict={X: train_X, Y:train_Y}) == oldCost or '%07f' % sess.run(w) == oldWeight or '%07f' % sess.run(b) == oldBias):
 			print '\n',sess.run(cost, feed_dict={X: train_X, Y:train_Y}) ,'==', oldCost ,'and', sess.run(w) ,'==', oldWeight ,'and', sess.run(b) ,'==', oldBias
@@ -132,8 +133,9 @@ with tf.Session() as sess:
 		oldArgs('%07f' % sess.run(cost, feed_dict={X: train_X, Y:train_Y}),'%07f' % sess.run(w),'%07f' % sess.run(b))
 
 	training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
-	print "Training cost=", training_cost, "W=", sess.run(w), "b=", sess.run(b), '\n'
+	print Fore.BLUE,"Training Cost:",Fore.RESET, training_cost,Fore.GREEN, "Training Weight:",Fore.RESET, sess.run(w),Fore.CYAN, "Training Bias:",Fore.RESET, sess.run(b), '\n'
 	plt.plot(train_X, train_Y, 'go', label='Original data')
+	print sess.run(cost, feed_dict={X: train_X, Y: train_Y,learning_rate: new_learning_rate})
 	# plt.plot(outliers.x_axis, outliers.y_axis, 'ro', label='Outlier data') 
 	plt.plot(train_X, sess.run(w) * train_X + sess.run(b),'b--', label='Fitted line')
 	plt.legend()
